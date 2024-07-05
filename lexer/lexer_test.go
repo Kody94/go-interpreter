646e859eda173c2b75253fca844e98d22428bd1b
Kody Striplin <kody.striplin@fawkesengineering.com>
2024-07-05 13:55:14 -0700

    add more keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -15,6 +15,8 @@ func TestNextToken(t *testing.T) {
 	};
 
 	let result = add(five, ten);
+	!-/*5;
+	5 < 10 > 5;
 	`
 	tests := []struct {
 		expectedType    token.TokenType
@@ -56,8 +58,21 @@ func TestNextToken(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
+
 	l := New(input)
 	for i, tt := range tests {
 		tok := l.NextToken()
