e309a028c46416155aa7c1fad43201d088d89435
Kody Striplin <kody.striplin@fawkesengineering.com>
2024-07-07 07:22:07 -0700

    start repl

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"example.com/monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello, %s! This is the Monkey Programming Language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\r")
+	repl.Start(os.Stdin, os.Stdout)
+}
